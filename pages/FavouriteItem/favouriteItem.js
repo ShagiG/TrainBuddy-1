0201d57cc1afe94765ef442a43fa05f3ffdbbd59
Kusal Hasintha Kithmal Kahaduwa <kusalhasintha2012@gmail.com>
2019-12-16 01:05:44 +0530

    add marker for google map

--- pages/FavouriteItem/favouriteItem.js
+++ pages/FavouriteItem/favouriteItem.js
@@ -17,8 +17,10 @@
 
 //initialize google map
 function initMap() {
+  //pass long and lat of the place
+  var myLatLng = { lat: 7.2906, lng: 80.6337 };
   var map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 7.2906, lng: 80.6337 },
+    center: myLatLng,
     zoom: 15,
     mapTypeControl: false
   });
@@ -39,6 +41,7 @@ function initMap() {
   var infowindowContent = document.getElementById("infowindow-content");
   infowindow.setContent(infowindowContent);
   var marker = new google.maps.Marker({
+    position: myLatLng,
     map: map,
     anchorPoint: new google.maps.Point(0, -29)
   });
@@ -67,60 +70,67 @@ function initMap() {
 }
 
 //Function for to give stars as reviews
-$(document).ready(function(){
-  
-    /* 1. Visualizing things on Hover - See next part for action on click */
-    $('#stars li').on('mouseover', function(){
-      var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
-     
+$(document).ready(function() {
+  /* 1. Visualizing things on Hover - See next part for action on click */
+  $("#stars li")
+    .on("mouseover", function() {
+      var onStar = parseInt($(this).data("value"), 10); // The star currently mouse on
+
       // Now highlight all the stars that's not after the current hovered star
-      $(this).parent().children('li.star').each(function(e){
-        if (e < onStar) {
-          $(this).addClass('hover');
-        }
-        else {
-          $(this).removeClass('hover');
-        }
-      });
-      
-    }).on('mouseout', function(){
-      $(this).parent().children('li.star').each(function(e){
-        $(this).removeClass('hover');
-      });
+      $(this)
+        .parent()
+        .children("li.star")
+        .each(function(e) {
+          if (e < onStar) {
+            $(this).addClass("hover");
+          } else {
+            $(this).removeClass("hover");
+          }
+        });
+    })
+    .on("mouseout", function() {
+      $(this)
+        .parent()
+        .children("li.star")
+        .each(function(e) {
+          $(this).removeClass("hover");
+        });
     });
-    
-    
-    /* 2. Action to perform on click */
-    $('#stars li').on('click', function(){
-      var onStar = parseInt($(this).data('value'), 10); // The star currently selected
-      var stars = $(this).parent().children('li.star');
-      
-      for (i = 0; i < stars.length; i++) {
-        $(stars[i]).removeClass('selected');
-      }
-      
-      for (i = 0; i < onStar; i++) {
-        $(stars[i]).addClass('selected');
-      }
-      
-      // JUST RESPONSE (Not needed)
-      var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
-      var msg = "";
-      if (ratingValue > 1) {
-          msg = "Thanks! You rated this " + ratingValue + " stars.";
-      }
-      else {
-          msg = "We will improve ourselves. You rated this " + ratingValue + " stars.";
-      }
+
+  /* 2. Action to perform on click */
+  $("#stars li").on("click", function() {
+    var onStar = parseInt($(this).data("value"), 10); // The star currently selected
+    var stars = $(this)
+      .parent()
+      .children("li.star");
+
+    for (i = 0; i < stars.length; i++) {
+      $(stars[i]).removeClass("selected");
+    }
+
+    for (i = 0; i < onStar; i++) {
+      $(stars[i]).addClass("selected");
+    }
+
+    // JUST RESPONSE (Not needed)
+    var ratingValue = parseInt(
+      $("#stars li.selected")
+        .last()
+        .data("value"),
+      10
+    );
+    var msg = "";
+    if (ratingValue > 1) {
+      msg = "Thanks! You rated this " + ratingValue + " stars.";
+    } else {
+      msg =
+        "We will improve ourselves. You rated this " + ratingValue + " stars.";
+    }
     //   responseMessage(msg);
-      
-    });
-    
-    
   });
-  
-  
+});
+
 //   function responseMessage(msg) {
-//     $('.success-box').fadeIn(200);  
+//     $('.success-box').fadeIn(200);
 //     $('.success-box div.text-message').html("<span>" + msg + "</span>");
 //   }
