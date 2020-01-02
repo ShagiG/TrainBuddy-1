a69c0ef74748f95156574067d4db05de36b2e846
thivatm <thivagartm@gmail.com>
2020-01-02 16:59:04 +0530

    Camera Debug

--- pages/Camera/js/main.js
+++ pages/Camera/js/main.js
@@ -238,9 +238,9 @@ function takeSnapshot() {
   getCanvasBlob(canvas).then(function(blob) {
     // do something with the image blob
     var image = canvas.toDataURL("image/png");
-    alert("Clicked at transfer !!");
-    window.localStorage.setItem("image", btoa(image));
-    window.location.replace = `/pages/PostPicture/index.html`;
+    alert("Clicked at transfer !");
+    // window.localStorage.setItem("image", btoa(image));
+    window.location.href = `/pages/PostPicture/index.html`;
   });
 }
 
