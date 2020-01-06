aa1ab20f9dad147ab9227163b27a82c12e6d199c
thivatm <thivagartm@gmail.com>
2020-01-06 15:06:39 +0530

    Minor change

--- pages/Homepage/home.js
+++ pages/Homepage/home.js
@@ -1,4 +1,19 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAqgAxxo3m2hpEGYi7l67WIV7VQ_G5hM88",
+  authDomain: "travelbuddy-261311.firebaseapp.com",
+  databaseURL: "https://travelbuddy-261311.firebaseio.com",
+  projectId: "travelbuddy-261311",
+  storageBucket: "travelbuddy-261311.appspot.com",
+  messagingSenderId: "955274868327",
+  appId: "1:955274868327:web:d21356955b0d832a42c6c2",
+  measurementId: "G-B649LJZBXP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 let feedback = {};
+//create firebase database reference
+let dbRef = firebase.database();
 
 $(() => {
   $("#submit").click(() => {
